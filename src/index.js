da2a3d1ac1a61c3c53e60778df4b2cd8deaea66e
hubcai <110979812+hubcai@users.noreply.github.com>
2025-11-12 10:27:31 +0800

    Update index.js
    修改域名信息

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.closeai.me": dockerHub,
-  "quay.closeai.me": "https://quay.io",
-  "gcr.closeai.me": "https://gcr.io",
-  "k8s-gcr.closeai.me": "https://k8s.gcr.io",
-  "k8s.closeai.me": "https://registry.k8s.io",
-  "ghcr.closeai.me": "https://ghcr.io",
-  "cloudsmith.closeai.me": "https://docker.cloudsmith.io",
+  "docker.caizb.top": dockerHub,
+  "quay.caizb.top": "https://quay.io",
+  "gcr.caizb.top": "https://gcr.io",
+  "k8s-gcr.caizb.top": "https://k8s.gcr.io",
+  "k8s.caizb.top": "https://registry.k8s.io",
+  "ghcr.caizb.top": "https://ghcr.io",
+  "cloudsmith.caizb.top": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.closeai.me": dockerHub,
+  "docker-staging.caizb.top": dockerHub,
 };
 
 function routeByHosts(host) {
